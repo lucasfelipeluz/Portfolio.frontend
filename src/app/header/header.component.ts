8e6b21f0eccad0a2ca45e28894699c7ca08324bb
Lucas Luz <65639478+lucasfelipeluz@users.noreply.github.com>
2022-01-08 13:10:37 -0300

    botao nav funcionando

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'header-component',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit{
 
+  @ViewChild('navButton') butaoNav: HTMLElement;
+  @ViewChild('menuMobile') menuMobile: ElementRef;
+  @ViewChildren('burgerButton') burgersButton: QueryList<object>;
+  
   constructor() { }
-
   ngOnInit(): void {
+    console.log('oninit')
   }
+
+  
+  private menu = {
+    open: false,
+  }
+
+  navMenu(): void {
+    if (this.menu.open === true) {
+      this.fecharMenu()
+    } else if (this.menu.open === false) {
+      this.abrirMenu()
+    }
+  }
+
+  abrirMenu(): void {
+    this.menu.open = true
+    this.menuMobile.nativeElement.dataset.menu = 'ativo';
+    this.burgersButton.forEach(burgerButton => {
+      let elementBurger: any = burgerButton;
+      elementBurger.nativeElement.dataset.filhoBurger = 'ativo'
+    })
+  }
+
+  fecharMenu(): void {
+    this.menu.open = false;
+    this.menuMobile.nativeElement.dataset.menu = 'inativo';
+    this.burgersButton.forEach(burgerButton => {
+      let elementBurger: any = burgerButton;
+      elementBurger.nativeElement.dataset.filhoBurger = 'inativo'
+    })
+  }
+
+
 
 }
